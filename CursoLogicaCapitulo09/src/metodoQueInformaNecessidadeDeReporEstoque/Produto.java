837547e8b771d97bc35dfa3267c0dde3e52a4a88
ikozielski <meidiaenove@gmail.com>
2024-05-03 18:22:48 -0300

    Exercicios Capitulo 9

--- CursoLogicaCapitulo09/src/metodoQueInformaNecessidadeDeReporEstoque/Produto.java
+++ CursoLogicaCapitulo09/src/metodoQueInformaNecessidadeDeReporEstoque/Produto.java
@@ -1,6 +1,23 @@
 package metodoQueInformaNecessidadeDeReporEstoque;
 
 public class Produto {
+	
+	static final Integer QUANTIDADE_MINIMA_DO_PRODUTO = 10;
+	
 	String nome;
 	Integer quantidadeEmEstoque;
+	
+	
+	 Boolean verificaQuantidadeDoProdutoNoEstoque() {
+
+		if (quantidadeEmEstoque < Produto.QUANTIDADE_MINIMA_DO_PRODUTO) {
+
+			return true;
+		}
+
+		return false;
+
+	}
 }
+
+
--- CursoLogicaCapitulo09/src/metodoQueInformaNecessidadeDeReporEstoque/VerificaNecessidadeDeReporEstoque.java
+++ CursoLogicaCapitulo09/src/metodoQueInformaNecessidadeDeReporEstoque/VerificaNecessidadeDeReporEstoque.java
@@ -2,26 +2,26 @@ package metodoQueInformaNecessidadeDeReporEstoque;
 
 public class VerificaNecessidadeDeReporEstoque {
 
-	static final Integer QUANTIDADE_MINIMA_DO_PRODUTO = 10;
-
 	public static void main(String[] args) {
 		Produto produto = new Produto();
 		produto.nome = "Marmita";
 		produto.quantidadeEmEstoque = 19;
 
-		System.out.println("Necessário repor estoque do produto " + produto.nome + "? " + verificaQuantidadeDoProdutoNoEstoque(produto));
+		System.out.println("Necessário repor estoque do produto " + produto.nome + "? " + produto.verificaQuantidadeDoProdutoNoEstoque());
 
 	}
 
-	static Boolean verificaQuantidadeDoProdutoNoEstoque(Produto produto) {
+	
 
-		if (produto.quantidadeEmEstoque < QUANTIDADE_MINIMA_DO_PRODUTO) {
-
-			return true;
-		}
-
-		return false;
-
-	}
+//	static Boolean verificaQuantidadeDoProdutoNoEstoque(Produto produto) {
+//
+//		if (produto.quantidadeEmEstoque < QUANTIDADE_MINIMA_DO_PRODUTO) {
+//
+//			return true;
+//		}
+//
+//		return false;
+//
+//	}
 
 }
