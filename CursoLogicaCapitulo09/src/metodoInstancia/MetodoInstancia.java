04a8a57549c67817356a97791adce6d9405ce11f
ikozielski <meidiaenove@gmail.com>
2024-05-03 17:39:29 -0300

    teste

--- CursoLogicaCapitulo09/src/metodoInstancia/MetodoInstancia.java
+++ CursoLogicaCapitulo09/src/metodoInstancia/MetodoInstancia.java
@@ -8,21 +8,19 @@ public class MetodoInstancia {
 		cliente.ultimoNome = "Afonso";
 		cliente.telefone = "44000000";
 		cliente.email = "alexandre.afonso@algawork.com";
-		
+
 		Cliente cliente2 = new Cliente();
 		cliente2.primeiroNome = "Iury";
 		cliente2.ultimoNome = "Kozielski";
 		cliente2.telefone = "34000000";
-		
-		System.out.println("Nome do cliente: " + cliente.obterNomeCompleto() + ",DDD: " + cliente.obterDDD()); 
-		System.out.println("Nome do cliente: " + cliente2.obterNomeCompleto() + ",DDD: " + cliente2.obterDDD()); 
+
+		System.out.println("Nome do cliente: " + cliente.obterNomeCompleto() + ",DDD: " + cliente.obterDDD());
+		System.out.println("Nome do cliente: " + cliente2.obterNomeCompleto() + ",DDD: " + cliente2.obterDDD());
 	}
-	
+
 //	static String obterNomeComprletoCliente (Cliente cliente) {
 //		String nomeCompleto = cliente.primeiroNome + " " + cliente.ultimoNome;
 //		return nomeCompleto;
 //	}
-	
-	
 
 }
